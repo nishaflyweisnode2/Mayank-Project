0a26a426a60f9dfda91f3b142e33d6a7475c413c
princekumar2022 <pk279183@gmail.com>
2024-07-15 13:10:04 +0530

    15 th july

--- controllers/partnerController.js
+++ controllers/partnerController.js
@@ -213,7 +213,7 @@ exports.verifyOtp = async (req, res) => {
 };
 exports.getProfile = async (req, res) => {
         try {
-                const data = await User.findOne({ _id: req.user._id, }).select('fullName email phone gender alternatePhone dob address1 address2 image refferalCode completeProfile city sector isCity isSector').populate('city sector');
+                const data = await User.findOne({ _id: req.user._id, }).populate('currentRole occupation role');
                 if (data) {
                         return res.status(200).json({ status: 200, message: "get Profile", data: data });
                 } else {
